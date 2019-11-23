c3864beba5afd2b33481c0fe4736038a45a2c105
dzhax499 <57004084+dzhax499@users.noreply.github.com>
2019-11-23 20:03:32 +0700

    Add files via upload

---
+++ whatshaxvip.sh
@@ -0,0 +1,27 @@
+clear
+black='\e[0;30m'
+blue='\e[0;34m'
+green='\e[0;32m'
+cyan='\e[0;36m'
+red='\e[0;31m'
+purple='\e[0;35m'
+brown='\e[0;33m'
+lightgray='\e[0;37m'
+darkgray='\e[1;30m'
+lightblue='\e[1;34m'
+lightgreen='\e[1;32m'
+lightcyan='\e[1;36m'
+lightred='\e[1;31m'
+lightpurple='\e[1;35m'
+yellow='\e[1;33m'
+white='\e[1;37m'
+
+echo -e $blue"Tools punya DzHax499"
+sleep 2
+echo -e $red"tools ini not free"
+sleep 2
+echo -e $green"Harus bayar 20k..."
+sleep 2
+echo -e $lightblue"kunjungi admin di nomor = 082111632833 no.admin"
+sleep 2
+exit
